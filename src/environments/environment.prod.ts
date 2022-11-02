66811a917d9518d739d0047790eb7230f58c71ee
Nicolas <nszchagas@gmail.com>
2022-11-02 18:14:55 -0300

    Deploy2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gitToken: ''
+  gitToken: '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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  gitToken: ''
+  gitToken: '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'
 };
